0d28d0301d5cccf3b0aba7d56788890eb88f785c
tatsu011 <cmcleod18@hotmail.com>
2013-04-27 18:24:31 -0400

    Constructed base Mod file and began the framework for reference files

---
+++ common/thermalDynamics/ThermalDynamics.java
@@ -0,0 +1,30 @@
+package thermalDynamics;
+
+import thermalDynamics.lib.refrences;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.event.*;
+
+
+@Mod( modid = refrences.MOD_ID, name = refrences.MOD_NAME , version = refrences.VERSION)
+public class ThermalDynamics
+{
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    
+    @Init
+    public void Load(FMLInitializationEvent event)
+    {
+        
+    }
+    
+    @PostInit
+    public void PostInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+
+}
---
+++ common/thermalDynamics/lib/refrences.java
@@ -0,0 +1,10 @@
+package thermalDynamics.lib;
+
+public class refrences
+{
+    public static final String MOD_ID = "ThermalDynamics";
+    public static final String MOD_NAME = "Thermal Dynamics";
+    public static final String VERSION = "0.0.1";
+    
+    
+}
